45ded98e0f84ba30761217cad9c1b0e0de46402c
ALADIN <aladin@iMac-de-ALADIN.local>
2024-08-05 12:59:45 +0000

    application de la deucième option

--- style.js
+++ style.js
@@ -7,12 +7,12 @@ function updateClock() {
     let h = new Date().getHours()
     let m = new Date().getMinutes()
     let s = new Date().getSeconds()
-    let ampm = "AM"
-    //let ampm = new Date().getHours() >= 12? "PM" : "AM"
-    if (h>=12){
-        h= h-12
-        ampm = "PM"
-    }
+    //let ampm = "AM"
+    let ampm = new Date().getHours() >= 12? "PM" : "AM"
+    //if (h>=12){
+    //    h= h-12
+    //    ampm = "PM"
+   // }
     h= h<10? "0"+h:h;
     m= m<10? "0"+m:m;
     s= s<10? "0"+s:s;
@@ -20,8 +20,8 @@ function updateClock() {
     hourEl.innerHTML = h
     minuteEl.innerHTML = m
     secondEl.innerHTML = s
+   // ampmEl.innerHTML = ampm
     ampmEl.innerHTML = ampm
-    //ampmEl.innerHTML = ampm
 
 }
 
